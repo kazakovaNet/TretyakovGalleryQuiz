72b13177fc29e4283c5a7990bf718639079d7206
kazakovaNet <kazakova.net@yandex.ru>
2018-01-26 17:58:43 +0300

    Added logic for displaying fragments with questions

--- app/src/main/java/com/example/android/tretyakovgalleryquiz/MainActivity.java
+++ app/src/main/java/com/example/android/tretyakovgalleryquiz/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.android.tretyakovgalleryquiz;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PictureAnswerFragment.PictureAnswerListener {
+    private int[] correctAnswers = new int[]{R.id.answer_1_button, R.id.answer_2_button, R.id.answer_3_button, R.id.answer_4_button, R.id.answer_1_button};
+    private int step = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PictureAnswerFragment pictureAnswerFragment = (PictureAnswerFragment) getFragmentManager().findFragmentById(R.id.picture_frag);
-        pictureAnswerFragment.setPicture(0);
+        setNewFragment();
+    }
+
+    @Override
+    public void onButtonClicked(long id, String correctAnswer) {
+        if (correctAnswers[step] != id) {
+            Toast.makeText(this, "You gave the wrong answer. Correct answer - " + correctAnswer, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "You are absolutely right!", Toast.LENGTH_SHORT).show();
+        }
+
+        if (++step < PictureAnswer.PICTURE_ANSWERS_DATA.length) {
+            setNewFragment();
+        } else {
+            Toast.makeText(this, "You answered all questions", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setNewFragment() {
+        PictureAnswerFragment fragment = new PictureAnswerFragment();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction(); // начало транзакции фрагмента
+
+        fragment.setAnswerData(step);
+        // Заменить фрагмент
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        // Добавить в стек возврата
+        fragmentTransaction.addToBackStack(null);
+        // Включить анимацию растворения и появления фрагментов
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        // Закрепить транзакцию
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/android/tretyakovgalleryquiz/PictureAnswerFragment.java
+++ app/src/main/java/com/example/android/tretyakovgalleryquiz/PictureAnswerFragment.java
@@ -2,7 +2,9 @@ package com.example.android.tretyakovgalleryquiz;
 
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +17,26 @@ import android.widget.TextView;
  * A simple {@link Fragment} subclass.
  */
 
-public class PictureAnswerFragment extends Fragment {
-    private int pictureId;
+public class PictureAnswerFragment extends Fragment implements View.OnClickListener {
+    private PictureAnswerListener listener;
+    private PictureAnswer pictureAnswer;
 
     public PictureAnswerFragment() {
         // Required empty public constructor
     }
+
+    public void onClick(View v) {
+        if (listener != null) {
+            // Сообщить слушателю о том, что на одной из кнопок был сделан щелчок
+            listener.onButtonClicked(v.getId(), pictureAnswer.getCorrectAnswer());
+        }
+    }
+
+    interface PictureAnswerListener {
+        void onButtonClicked(long id, String correctAnswer);
+    }
+
+    ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,30 +49,49 @@ public class PictureAnswerFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
+        initializeFragment();
+    }
+
+    private void initializeFragment() {
         View view = getView();
+
         if (view != null) {
-            Picture picture = Picture.pictures[pictureId];
-            String[] answers = picture.getAnswers();
-            String correctAnswer = answers[picture.getCorrectAnswer()];
+            String[] answers = pictureAnswer.getAnswers();
 
             ImageView pictureImageView = view.findViewById(R.id.picture_image_view);
+            pictureImageView.setImageResource(pictureAnswer.getPictureId());
+
             TextView questionTextView = view.findViewById(R.id.question_text_view);
+            questionTextView.setText(pictureAnswer.getQuestion());
+
+            // Получение ссылок на кнопки
             Button answer1Button = view.findViewById(R.id.answer_1_button);
             Button answer2Button = view.findViewById(R.id.answer_2_button);
             Button answer3Button = view.findViewById(R.id.answer_3_button);
             Button answer4Button = view.findViewById(R.id.answer_4_button);
 
-            pictureImageView.setImageResource(picture.getPictureId());
-            questionTextView.setText(picture.getQuestion());
+            // Назначение слушателей и текста кнопкам
+            answer1Button.setOnClickListener(this);
             answer1Button.setText(answers[0]);
+            answer2Button.setOnClickListener(this);
             answer2Button.setText(answers[1]);
+            answer3Button.setOnClickListener(this);
             answer3Button.setText(answers[2]);
+            answer4Button.setOnClickListener(this);
             answer4Button.setText(answers[3]);
         }
-
     }
 
-    public void setPicture(int pictureId) {
-        this.pictureId = pictureId;
+    // Вызывается при присоединении фрагмента к активности
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        this.listener = (PictureAnswerListener) context;
+    }
+
+    // Определяются данные для отображения
+    public void setAnswerData(int pictureId) {
+        pictureAnswer = PictureAnswer.PICTURE_ANSWERS_DATA[pictureId];
     }
 }
