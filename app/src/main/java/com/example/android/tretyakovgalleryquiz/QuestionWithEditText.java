476bada509327626d80ef38ab7f024e0b0882ba1
kazakovaNet <kazakova.net@yandex.ru>
2018-02-05 12:16:22 +0300

    Found a solution to the problem (todo)

--- app/src/main/java/com/example/android/tretyakovgalleryquiz/QuestionWithEditText.java
+++ app/src/main/java/com/example/android/tretyakovgalleryquiz/QuestionWithEditText.java
@@ -5,4 +5,9 @@ class QuestionWithEditText extends Question {
     QuestionWithEditText(int questionId, int pictureImageId, int correctAnswerId) {
         super(questionId, pictureImageId, correctAnswerId);
     }
+
+    /*TODO
+    QuestionWithEditText(int questionId, int pictureImageId, int correctAnswerId) {
+        super(pictureImageId, correctAnswerId, questionId);
+    }*/
 }
