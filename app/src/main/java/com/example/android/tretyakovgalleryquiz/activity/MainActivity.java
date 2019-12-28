454504f3458e3813bf9511f336d10c2a98add37d
KazakovaNet <kazakovaNet@yandex.ru>
2019-12-28 13:21:09 +0300

    Update gradle and library versions

--- app/src/main/java/com/example/android/tretyakovgalleryquiz/activity/MainActivity.java
+++ app/src/main/java/com/example/android/tretyakovgalleryquiz/activity/MainActivity.java
@@ -6,12 +6,13 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.android.tretyakovgalleryquiz.fragment.QuestionWithCheckBoxFragment;
 import com.example.android.tretyakovgalleryquiz.helper.AlertHelper;
--- app/src/main/java/com/example/android/tretyakovgalleryquiz/fragment/DialogAlertFragment.java
+++ app/src/main/java/com/example/android/tretyakovgalleryquiz/fragment/DialogAlertFragment.java
@@ -6,12 +6,13 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.android.tretyakovgalleryquiz.R;
 import com.example.android.tretyakovgalleryquiz.activity.MainActivity;
